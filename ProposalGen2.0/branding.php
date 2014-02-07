72588869aaf12f6e3c7744c1397b0da6fb73fbf1
Nickkos <Nickkoskowski@gmail.com>
2014-02-07 00:52:42 -0800

    Added V2.0 Update
    Printable Docs and PDF Generation

---
+++ ProposalGen2.0/branding.php
@@ -0,0 +1,79 @@
+<section id="branding" class="screen discipline">
+		<div class="container">
+			<h2 class="dark-treatment">Branding</h2>
+			<img class="icon" src="images/brand-icon.png">
+			<div class="clearfix"></div>
+			<div class="third-column">
+				<p>
+					<b>Planning</b>
+				</p>
+				<ul>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+				</ul>
+				<p>
+					<b>Planning</b>
+				</p>
+				<ul>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+				</ul>
+			</div>
+			<div class="third-column">
+				<p>
+					<b>Planning</b>
+				</p>
+				<ul>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+				</ul>
+				<p>
+					<b>Planning</b>
+				</p>
+				<ul>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+				</ul>
+			</div>
+			<div class="third-column">
+				<p>
+					<b>Planning</b>
+				</p>
+				<ul>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+				</ul>
+				<p>
+					<b>Planning</b>
+				</p>
+				<ul>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+				</ul>
+			</div>
+		</div>
+	</section>
---
+++ ProposalGen2.0/research-and-development.php
@@ -0,0 +1,55 @@
+<section id="research-and-planning" class="screen discipline">
+		<div class="container">
+			<h2 class="dark-treatment">Research and Planning</h2>
+			<img class="icon" src="images/rd-icon.png">
+			<div class="clearfix"></div>
+			<div class="half-column">
+				<p>
+					<b>Planning</b>
+				</p>
+				<ul>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+				</ul>
+				<p>
+					<b>Planning</b>
+				</p>
+				<ul>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+				</ul>
+			</div>
+			<div class="half-column">
+				<p>
+					<b>Planning</b>
+				</p>
+				<ul>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+				</ul>
+				<p>
+					<b>Planning</b>
+				</p>
+				<ul>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+					<li>Due Dilligence Research</li>
+				</ul>
+			</div>
+		</div>
+	</section>
